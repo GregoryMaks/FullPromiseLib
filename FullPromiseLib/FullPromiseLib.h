2341c5bf6bc225a32402060c65bdb6af67d3f5ed
Gregory Maksyuk <gregory.maksyuk@gmail.com>
2017-11-27 00:36:19 +0200

    Initial Commit

---
+++ FullPromiseLib/FullPromiseLib.h
@@ -0,0 +1,19 @@
+//
+//  FullPromiseLib.h
+//  FullPromiseLib
+//
+//  Created by Gregory Maksyuk on 11/27/17.
+//  Copyright © 2017 Gregory Maksyuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FullPromiseLib.
+FOUNDATION_EXPORT double FullPromiseLibVersionNumber;
+
+//! Project version string for FullPromiseLib.
+FOUNDATION_EXPORT const unsigned char FullPromiseLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FullPromiseLib/PublicHeader.h>
+
+
